c3200f9a2276b116a12a2199419603c6b66a40ed
bqnce <csobadibence@proton.me>
2026-03-24 21:44:59 +0100

    Add @vercel/speed-insights package and integrate SpeedInsights component in layout for performance optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Sora } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 // Sora font beállítása
@@ -17,6 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="hu">
+      <SpeedInsights />
+
       {/* Itt cseréltük a class-t a sora.className-re */}
       <body className={sora.className}>
         <main className="min-h-screen">
